befe15b03e86ba609a0ba07be4ee4d572b79245d
MetehanKoray <mkoray41@hotmail.com>
2018-07-29 03:28:06 +0300

    StopWatch Time

---
+++ StopWatch/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace stopWatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            for (int i = 1; i <=10 ; i++)
+            {
+                for (int j = 0; j <i ; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+            int a = 0;
+            int b = 1;
+            
+
+            for (int i = 1; i <= 1; i++)
+            {
+                Console.Write(a + ",");
+                Console.Write(b + ",");
+            }
+            for (int i = 1; i <=10; i++)
+            {
+                int toplam = a + b;
+                a = b;
+                b = toplam;
+                Console.Write(toplam+",");
+            }
+            Console.WriteLine();
+            watch.Stop();
+            Console.WriteLine("Stop Süresi: "+watch.Elapsed.Milliseconds);
+
+            Console.ReadLine();
+        }
+    }
+}
